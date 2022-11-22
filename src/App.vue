542dd7683b36ef6d57a1c815811e1e9114f40651
Oskar Mularski <oskarmularski98@gmail.com>
2022-11-22 21:10:10 +0100

    added loading

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,61 @@
 <template>
   <v-app>
-    <!-- <v-navigation-drawer v-model="drawer" app>
-    </v-navigation-drawer> -->
-
     <v-app-bar app>
       <v-spacer></v-spacer>
       <v-toolbar-title>Public APIs</v-toolbar-title>
       <v-spacer></v-spacer>
     </v-app-bar>
-
     <v-main>
       <v-container>
         <v-row>
           <v-col cols="12">
-            <v-data-table
-              :headers="headers"
-              :items="apis"
-              hide-default-footer
-              :page.sync="page"
-              :search="search"
-              :items-per-page="itemsPerPage"
-              @page-count="pageCount = $event"
-            >
-              <template #top>
-                <v-col cols="3">
-                  <v-text-field label="Search" v-model="search"></v-text-field>
-                </v-col>
-              </template>
-              <template #[`item.HTTPS`]="{ item }">
-                <div v-if="item.HTTPS === true">
-                  <v-icon color="success"> {{ "mdi-check" }} </v-icon>
-                </div>
-                <div v-else>
-                  <v-icon color="red"> {{ "mdi-close" }} </v-icon>
-                </div>
-              </template>
-            </v-data-table>
-            <v-pagination
-              v-model="page"
-              :length="pageCount"
-              :total-visible="10"
-            ></v-pagination>
+            <div v-if="loading" class="text-center">
+              <v-progress-circular
+                indeterminate
+                color="primary"
+              ></v-progress-circular>
+            </div>
+            <v-fade-transition hide-on-leave>
+              <div v-if="!loading">
+                <v-data-table
+                  :headers="headers"
+                  :items="apis"
+                  hide-default-footer
+                  :page.sync="page"
+                  :search="search"
+                  :items-per-page="itemsPerPage"
+                  @page-count="pageCount = $event"
+                >
+                  <template #top>
+                    <v-col cols="3">
+                      <v-text-field
+                        label="Search"
+                        v-model="search"
+                      ></v-text-field>
+                    </v-col>
+                  </template>
+                  <template #[`item.HTTPS`]="{ item }">
+                    <div v-if="item.HTTPS === true">
+                      <v-icon color="success"> {{ "mdi-check" }} </v-icon>
+                    </div>
+                    <div v-else>
+                      <v-icon color="red"> {{ "mdi-close" }} </v-icon>
+                    </div>
+                  </template>
+                  <template #[`item.Link`]="{ item }">
+                    <a :href="item.Link" target="_blank">{{ item.Link }}</a>
+                  </template>
+                </v-data-table>
+                <v-pagination
+                  v-model="page"
+                  :length="pageCount"
+                  :total-visible="10"
+                ></v-pagination>
+              </div>
+            </v-fade-transition>
           </v-col>
         </v-row>
       </v-container>
-      <!--  -->
     </v-main>
   </v-app>
 </template>
@@ -57,6 +68,7 @@ export default {
     return {
       apis: [],
       search: "",
+      loading: false,
       headers: [
         {
           text: "API",
@@ -67,36 +79,36 @@ export default {
           text: "Auth",
           value: "Auth",
           sortable: false,
-          filterable: false
+          filterable: false,
         },
         {
           text: "Category",
           value: "Category",
-          filterable: false
+          filterable: false,
         },
         {
           text: "Cors",
           value: "Cors",
           sortable: false,
-          filterable: false
+          filterable: false,
         },
         {
           text: "Description",
           value: "Description",
           sortable: false,
-          filterable: false
+          filterable: false,
         },
         {
           text: "HTTPS",
           value: "HTTPS",
           sortable: false,
-          filterable: false
+          filterable: false,
         },
         {
           text: "Link",
           value: "Link",
           sortable: false,
-          filterable: false
+          filterable: false,
         },
       ],
       length: null,
@@ -107,7 +119,9 @@ export default {
     };
   },
   async created() {
+    this.loading = true;
     await this.loadApis();
+    this.loading = false;
   },
   methods: {
     async loadApis() {
