dae279c34a9966a0547527e492f194b12776e01c
Oskar Mularski <oskarmularski98@gmail.com>
2022-11-22 20:59:22 +0100

    created pagination + data table

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,125 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <!-- <v-navigation-drawer v-model="drawer" app>
+    </v-navigation-drawer> -->
+
+    <v-app-bar app>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Public APIs</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-data-table
+              :headers="headers"
+              :items="apis"
+              hide-default-footer
+              :page.sync="page"
+              :search="search"
+              :items-per-page="itemsPerPage"
+              @page-count="pageCount = $event"
+            >
+              <template #top>
+                <v-col cols="3">
+                  <v-text-field label="Search" v-model="search"></v-text-field>
+                </v-col>
+              </template>
+              <template #[`item.HTTPS`]="{ item }">
+                <div v-if="item.HTTPS === true">
+                  <v-icon color="success"> {{ "mdi-check" }} </v-icon>
+                </div>
+                <div v-else>
+                  <v-icon color="red"> {{ "mdi-close" }} </v-icon>
+                </div>
+              </template>
+            </v-data-table>
+            <v-pagination
+              v-model="page"
+              :length="pageCount"
+              :total-visible="10"
+            ></v-pagination>
+          </v-col>
+        </v-row>
+      </v-container>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      apis: [],
+      search: "",
+      headers: [
+        {
+          text: "API",
+          value: "API",
+          sortable: false,
+        },
+        {
+          text: "Auth",
+          value: "Auth",
+          sortable: false,
+          filterable: false
+        },
+        {
+          text: "Category",
+          value: "Category",
+          filterable: false
+        },
+        {
+          text: "Cors",
+          value: "Cors",
+          sortable: false,
+          filterable: false
+        },
+        {
+          text: "Description",
+          value: "Description",
+          sortable: false,
+          filterable: false
+        },
+        {
+          text: "HTTPS",
+          value: "HTTPS",
+          sortable: false,
+          filterable: false
+        },
+        {
+          text: "Link",
+          value: "Link",
+          sortable: false,
+          filterable: false
+        },
+      ],
+      length: null,
+      page: 1,
+      pageCount: 0,
+      itemsPerPage: 10,
+      total: null,
+    };
+  },
+  async created() {
+    await this.loadApis();
+  },
+  methods: {
+    async loadApis() {
+      try {
+        const response = await axios.get("https://api.publicapis.org/entries");
+        this.total = response.data.count;
+        this.apis = response.data.entries;
+        this.length = Math.ceil(this.total / this.itemsPerPage);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
